933b64ed663cce796b66223c10d0ac283415c97b
qiaogeer <937468576@qq.com>
2020-02-17 13:38:23 +0800

    add header

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+        <i class="iconfont back-icon" >&#xe624;</i>
+    </div>
+    <div class="header-input">
+        <i class="iconfont">&#xe632;</i>
+        输入城市/景点/游玩主题</div>
+    <div class="header-right">
+        城市
+        <i class="iconfont arrow-icon">&#xe600;</i>
+        </div>
+  </div>
+</template>
+<script>
+
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .header
+    display :flex
+    height: .86rem
+    background: $bgColor
+    color:#fff
+    line-height .86rem
+    .header-left
+      width: .64rem
+      float:left
+      .back-icon
+        display :block
+        text-align :center
+        font-size:.4rem
+    .header-input
+      flex: 1
+      background:#fff
+      margin-top:.12rem
+      margin-left:.2rem
+      padding-left:.2rem
+      height:.62rem
+      border-radius:.1rem
+      color:#ccc
+      line-height:.62rem
+    .header-right
+      width: 1.24rem
+      float:right
+      text-align:center
+      .arrow-icon
+        font-size:.24rem
+        margin-left:-.04rem
+</style>
