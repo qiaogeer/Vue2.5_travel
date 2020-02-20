00d8923c1b74aaca56ba3e860e780ecf599872d8
qiaogeer <937468576@qq.com>
2020-02-20 10:19:47 +0800

    city ajax

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -21,65 +21,18 @@
     <div class="area">
       <div class="title border-topbottom">热门城市</div>
       <div class="button-list">
-      <div class="button-wrapper">
-          <div class="button">北京</div>
-          </div>
-          <div class="button-wrapper">
-          <div class="button">北京</div>
-          </div>
-          <div class="button-wrapper">
-          <div class="button">北京</div>
-          </div>
-          <div class="button-wrapper">
-          <div class="button">北京</div>
-          </div>
-          </div>
-    </div>
-    <div class="area">
-      <div class="title border-topbottom">A</div>
-      <div class="item-list">
-        <div class="item border-bottom ">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
+      <div class="button-wrapper" v-for="item in hot" :key="item.id">
+          <div class="button">{{item.name}}</div>
+      </div>
       </div>
     </div>
-    <div class="area">
-      <div class="title border-topbottom">A</div>
-      <div class="item-list">
-        <div class="item border-bottom ">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
+    <div class="area" v-for="(item,key) in cities" :key="key">
+      <div class="title border-topbottom">{{key}}</div>
+      <div class="item-list" v-for="city in item" :key='city.id'>
+        <div class="item border-bottom ">{{city.name}}</div>
       </div>
     </div>
-    <div class="area">
-      <div class="title border-topbottom">A</div>
-      <div class="item-list">
-        <div class="item border-bottom ">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-      </div>
-    </div>
-    <div class="area">
-      <div class="title border-topbottom">A</div>
-      <div class="item-list">
-        <div class="item border-bottom ">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-      </div>
-    </div>
-    </div>
+  </div>
   </div>
 </template>
 
@@ -89,6 +42,10 @@ export default {
   name:'CityList',
   mounted(){
     this.scroll=new Bscroll(this.$refs.wrapper)
+  },
+  props:{
+    cities: Object,
+    hot: Array
   }
 }
 </script>
