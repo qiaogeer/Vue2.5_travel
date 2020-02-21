0dd5bff7e2a574fcba609396f2b031634712b6e9
qiaogeer <937468576@qq.com>
2020-02-21 10:06:16 +0800

    finish page

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <home-header></home-header>
-    <home-swiper :list='swiperList'></home-swiper>
-    <home-icons :list='iconList'></home-icons>
-    <home-recommend :list='recommendList'></home-recommend>
-    <home-weekend :list='weekendList'></home-weekend>
+    <home-swiper :list="swiperList"></home-swiper>
+    <home-icons :list="iconList"></home-icons>
+    <home-recommend :list="recommendList"></home-recommend>
+    <home-weekend :list="weekendList"></home-weekend>
   </div>
 </template>
 
@@ -15,6 +15,7 @@ import HomeIcons from './components/Icons'
 import HomeRecommend from './components/Recommend'
 import HomeWeekend from './components/Weekend'
 import axios from 'axios'
+import {mapState} from 'vuex'
 export default {
   name: 'Home',
   components: {
@@ -26,18 +27,29 @@ export default {
   },
   data() {
     return {
+      lastCity : '',
       swiperList: [],
       iconList: [],
       recommendList: [],
       weekendList: []
     }
   },
+  computed:{
+    ...mapState(['city'])
+  },
   mounted() {
+    this.lastCity = this.city
     this.getHomeInfo()
+  },
+  activated () {
+    if (this.lastCity !== this.city) {
+      this.lastCity = this.city
+      this.getHomeInfo()
+    }
   },
   methods: {
     getHomeInfo() {
-      axios.get('/api/index.json')
+      axios.get('/api/index.json?city=' + this.city)
         .then(this.getHomeInfoSucc)
     },
     getHomeInfoSucc(res) {
